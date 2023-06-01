58f30943bedc5f94808c7b077a2428a386bde8d6
lab <user@IFPR-Lab.localdomain>
2023-06-01 12:38:01 -0300

    Initial commit

---
+++ app/src/main/java/com/example/projetopdm2023/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.projetopdm2023;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.Button;
+public class MainActivity extends AppCompatActivity {
+
+    public EditText Nomeusuario;
+    public EditText Numerouser;
+    public Button Registrar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
