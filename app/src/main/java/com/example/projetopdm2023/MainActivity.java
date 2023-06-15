9172c2d487e77eadb6c47661cc33e43a1c3c3b07
ifpr <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>
2023-06-15 12:09:38 -0300

    ta tudo indo

--- app/src/main/java/com/example/projetopdm2023/MainActivity.java
+++ app/src/main/java/com/example/projetopdm2023/MainActivity.java
@@ -4,13 +4,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 import android.widget.Button;
+import javax.security.auth.callback.PasswordCallback;
+
 public class MainActivity extends AppCompatActivity {
 
     public EditText Nomeusuario;
+    public EditText Sera;
     public Button Registrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Nomeusuario = findViewById(R.id.Nomeusuario);
+        Sera = findViewById(R.id.Sera);
+        Registrar = findViewById(R.id.Registrar);
     }
 }
